9240a5c6b0b2de13c20b3db7a1ce0c26fdcd860b
Russell Neches <ryneches@ucdavis.edu>
2010-10-29 03:54:21 -0700

    fixed spelling mistake

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ except ImportError:
     print '(WARNING: importing distutils, not setuptools!)'
     from distutils.core import setup
 
-setup(name='debrujinator',
+setup(name='debruijnator',
       version='0.1',
-      description='A basic de Brujin graph implementation',
+      description='A basic de Bruijn graph implementation',
       author='Russell Neches',
       author_email='ryneches@ucdavis.edu',
       url='',
-      packages=['debrujinator', 'debrujinator.tests'],
-      package_data={'debrujinator.tests': ['stap_16S.fa', 'query.fa']},
+      packages=['debruijnator', 'debrujinator.tests'],
+      package_data={'debruijnator.tests': ['stap_16S.fa', 'query.fa']},
       license='BSD',
       test_suite = 'nose.collector'
       )
